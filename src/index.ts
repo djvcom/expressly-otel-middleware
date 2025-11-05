e5537971b09b7e9cc0a86d32bf9f2c58415917bc
Daniel Verrall <djv3@student.london.ac.uk>
2025-11-05 21:18:33 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="@fastly/js-compute" />
+
+import { Router } from "@fastly/expressly"
+
+const router = new Router();
+
+router.get("/", async (req, res) => {
+  return res.send("Hello world!");
+});
+
+router.listen();
